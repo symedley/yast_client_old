1c4c01d42c4b27dc38a5ce5383c4177b7dfa1ffb
susannahmedley <symedley@gmail.com>
2018-09-23 14:41:43 -0700

    feat: works on iOS simulator. feat: add Cancel button to the login page since iOS has no back button. bug: login screen username kept reverting to last value. Fixed.
    Part of hooking up Firebase Cloudstore: edit Podfile, run pod update. (Cocoa beans) Add both build.gradle files to git.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,18 +10,22 @@ class LoginPage extends StatefulWidget {
 
   static String tag = 'login-page';
   final SavedAppStatus
-  theSavedState; // this is a reference back to the main level saved state.
+      theSavedState; // this is a reference back to the main level saved state.
 
   @override
-  _LoginPageState createState() => new _LoginPageState(this.theSavedState.getUsername());
+  _LoginPageState createState() =>
+      new _LoginPageState(this.theSavedState.getUsername());
 }
 
 class _LoginPageState extends State<LoginPage> {
-  _LoginPageState(this.username);
+  _LoginPageState(this.username){
+    usernameTextController = TextEditingController();
+    usernameTextController.text = username;
+  }
 
   String username; // saves the latest username to shared preferences.
   final passwdTextController = TextEditingController();
-  final usernameTextController = TextEditingController();
+  TextEditingController usernameTextController ;
   bool _loginInProgress;
 
   @override
@@ -34,7 +38,6 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    usernameTextController.text = (username == null ? "" : username);
     _scaffoldContext = context;
     _loginInProgress = false;
     final email = TextFormField(
@@ -51,6 +54,7 @@ class _LoginPageState extends State<LoginPage> {
 
     final password = TextFormField(
       controller: passwdTextController,
+      keyboardType: TextInputType.text,
       autofocus: true,
       initialValue: null,
       obscureText: true,
@@ -63,72 +67,67 @@ class _LoginPageState extends State<LoginPage> {
 
     final loginButton = Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        borderRadius: BorderRadius.circular(30.0),
-        shadowColor: Colors.lightBlueAccent.shade100,
-        elevation: 5.0,
-        child: MaterialButton(
-          minWidth: 200.0,
-          height: 42.0,
-          onPressed: () async {
-            var usernameTextInput = Text(usernameTextController.text);
+      child: MaterialButton(
+        minWidth: 200.0,
+        height: 42.0,
+        onPressed: () async {
+          var usernameTextInput = Text(usernameTextController.text);
+          try {
+            username = usernameTextInput.data;
+          } catch (e) {
+            username = '';
+          }
+
+          widget.theSavedState.setUsername(username);
+
+          if (_loginInProgress != true) {
             try {
-              username = usernameTextInput.data;
-            } catch (e) {
-              username = '';
-            }
+              var passwdTextInput = Text(passwdTextController.text);
+              var pw = passwdTextInput.data;
+              final snackBar = SnackBar(
+                content: Text('Please wait...'),
+                action: SnackBarAction(
+                  label: 'OK',
+                  onPressed: () {
+                    // Some code to undo the change!
+                  },
+                ),
+              );
+              Scaffold.of(_scaffoldContext).showSnackBar(snackBar);
 
-            widget.theSavedState.setUsername(username);
+              _loginInProgress = true;
+              String retval = await attemptLogin(pw);
 
-            if (_loginInProgress != true) {
-              try {
-                var passwdTextInput = Text(passwdTextController.text);
-                var pw = passwdTextInput.data;
+              if (retval != null) {
+                _loginInProgress = false;
+                Navigator.pop(context, retval);
+              } else {
+                // retval is null, so the HTTP login request failed for unknown reason
+                _loginInProgress = false;
+
                 final snackBar = SnackBar(
-                  content: Text('Please wait...'),
+                  content: Text('Login failed'),
+                  duration: Duration(seconds: 10),
                   action: SnackBarAction(
                     label: 'OK',
-                    onPressed: () {
-                      // Some code to undo the change!
-                    },
+                    onPressed: () {},
                   ),
                 );
                 Scaffold.of(_scaffoldContext).showSnackBar(snackBar);
-
-                _loginInProgress = true;
-                String retval = await attemptLogin(pw);
-
-                if (retval != null) {
-                  _loginInProgress = false;
-                  Navigator.pop(context, retval);
-                } else {
-                  // retval is null, so the HTTP login request failed for unknown reason
-                  _loginInProgress = false;
-
-                  final snackBar = SnackBar(
-                    content: Text('Login failed'),
-                    duration: Duration(seconds: 10),
-                    action: SnackBarAction(
-                      label: 'OK',
-                      onPressed: () {},
-                    ),
-                  );
-                  Scaffold.of(_scaffoldContext).showSnackBar(snackBar);
-                }
-              } on TimeoutException catch (e) {
-                debugPrint("Login timed out: $e");
-                showSnackbar(_scaffoldContext, 'Login timed out.');
-                _loginInProgress = false;
               }
-            } else {
-              debugPrint('Login already in progress when login button clicked');
-              showSnackbar(_scaffoldContext, 'Login already in progress.');
+            } on TimeoutException catch (e) {
+              debugPrint("Login timed out: $e");
+              showSnackbar(_scaffoldContext, 'Login timed out.');
+              _loginInProgress = false;
             }
-            //Navigator.of(context).pop(retval);
-          },
-          color: Colors.lightBlueAccent,
-          child: Text('Log In', style: TextStyle(color: Colors.white)),
-        ),
+          } else {
+            debugPrint('Login already in progress when login button clicked');
+            showSnackbar(_scaffoldContext, 'Login already in progress.');
+          }
+          //Navigator.of(context).pop(retval);
+        },
+        color: Colors.lightBlueAccent,
+        child: Text('Log In', style: TextStyle(color: Colors.white)),
       ),
     );
 
@@ -140,14 +139,28 @@ class _LoginPageState extends State<LoginPage> {
       onPressed: () {},
     );
 
+    final cancelButton = FlatButton(
+      child: Text(
+        'Cancel',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {
+        _loginInProgress = false;
+        Navigator.pop(context, null);
+      },
+    );
+
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: new Builder(builder: (BuildContext context) {
-          _scaffoldContext = context;
-          return Center(
-            child: ListView(
-              shrinkWrap: true,
-              padding: EdgeInsets.only(left: 24.0, right: 24.0),
+      backgroundColor: Colors.white,
+      body: new Builder(builder: (BuildContext context) {
+        _scaffoldContext = context;
+        return Padding(
+          padding: const EdgeInsets.only(
+              left: 8.0, top: 80.0, right: 8.0, bottom: 20.0),
+          child: Container(
+            alignment: Alignment.topCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 SizedBox(height: 48.0),
                 email,
@@ -155,11 +168,14 @@ class _LoginPageState extends State<LoginPage> {
                 password,
                 SizedBox(height: 24.0),
                 loginButton,
-                forgotLabel
+                forgotLabel,
+                cancelButton,
               ],
             ),
-          );
-        }));
+          ),
+        );
+      }),
+    );
   }
 
   /// NOw try to log in with the password the user just entered.
